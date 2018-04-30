68d7bd1e0bccd7a696c6ae08d41ba321b68a41ef
Miguel Ferrer <miguef7@gmail.com>
2018-04-30 16:04:16 -0500

    add helper to handle users without picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def profile_pic_or_generic(user)
+    if user.photo.blank?
+      cl_image_tag "user.png",
+      gravity: "center",
+      height: 300,
+      width: 300,
+      crop: "fit",
+      class: "avatar dropdown-toggle",
+      id: "navbar-wagon-menu",
+      "data-toggle" => "dropdown"
+    else
+      cl_image_tag user.photo,
+        height: 300,
+        width: 300,
+        crop: :fill,
+        gravity: :face,
+        class: "avatar dropdown-toggle",
+        id: "navbar-wagon-menu",
+        "data-toggle" => "dropdown"
+    end
+  end
 end
