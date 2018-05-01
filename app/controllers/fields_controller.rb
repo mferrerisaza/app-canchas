32e64bcffe564a43b03c312627ea56049452a7c8
Miguel Ferrer <miguef7@gmail.com>
2018-04-30 21:37:55 -0500

    fix bugs

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -3,13 +3,13 @@ skip_before_action :authenticate_user!, only: [:index, :show]
 
 
   def index
-    @business = policy_scope(Business.where.not(latitude: nil, longitude: nil))
-    @fields = Field.all.map { |field| field if @business.include?(field.business) }
+    @businesses = policy_scope(Business.where.not(latitude: nil, longitude: nil))
+    @fields = Field.all.select { |field| @businesses.include? (field.business) }
 
     @markers = @fields.map do |field|
       {
         lat: field.business.latitude,
-        lng: field.business.longitude#,
+        lng: field.business.longitude,
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }
     end
