6d72585f74243a295188c5215515c1986e23ebbd
Miguel Ferrer <miguef7@gmail.com>
2018-05-01 18:12:29 -0500

    add flatplicker for start and endtime to set default opening hours

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -17,13 +17,28 @@ flatpickr(".datepicker", {
   })]
 });
 
-flatpickr(".timepicker", {
+flatpickr("#starttimepicker", {
   altInput: true,
   enableTime: true,
   noCalendar: true,
   defaultHour: 17,
   hourIncrement: 1,
-  minuteIncrement: 30,
+  minuteIncrement: 60,
+  plugins: [new confirmDatePlugin({
+    confirmIcon: "<i class='fa fa-check'></i>",
+    confirmText: "OK ",
+    showAlways: true,
+    theme: "light"
+  })]
+});
+
+flatpickr("#endtimepicker", {
+  altInput: true,
+  enableTime: true,
+  noCalendar: true,
+  defaultHour: 20,
+  hourIncrement: 1,
+  minuteIncrement: 60,
   plugins: [new confirmDatePlugin({
     confirmIcon: "<i class='fa fa-check'></i>",
     confirmText: "OK ",
