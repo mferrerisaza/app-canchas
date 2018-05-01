02a26de110e6bf9d6f9d82dca6febdb24942dda4
Daniel Lopez <dlopez.10g@gmail.com>
2018-04-30 22:09:38 -0500

    horas disponibles working

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,11 +1,10 @@
 class FieldsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
-
+require 'byebug'
 
   def index
     @business = policy_scope(Business.where.not(latitude: nil, longitude: nil))
     @fields = Field.all.map { |field| field if @business.include?(field.business) }
-
     @markers = @fields.map do |field|
       {
         lat: field.business.latitude,
@@ -13,6 +12,8 @@ skip_before_action :authenticate_user!, only: [:index, :show]
         # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
       }
     end
+
+
   end
 
   def show
