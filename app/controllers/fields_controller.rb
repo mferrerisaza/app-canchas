7c503ea5abc7511b144045463fd59c2c0fc99f87
Miguel Ferrer <miguef7@gmail.com>
2018-05-01 18:11:42 -0500

    add actions for schedule building

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,9 +1,12 @@
 class FieldsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:index, :show]
+skip_before_action :authenticate_user!, only: [:index, :show, :test]
+after_action :verify_authorized, except: [:index, :test]
 
   def index
     @businesses = policy_scope(Business.where.not(latitude: nil, longitude: nil))
     @fields = Field.all.decorate #FieldDecorator.decorate_collection(Field.all.select { |field| @businesses.include?(field.business) })
+    # @schedule = build_schedule_filtered
+
     @markers = @businesses.map do |business|
       {
         lat: business.latitude,
@@ -22,5 +25,26 @@ skip_before_action :authenticate_user!, only: [:index, :show]
 
   def create
     authorized @field
+  end
+
+  def schedule
+    dates = params[:dates].split("to").map { |date| Date.parse(date) }
+    start_hour = params[:start_time].to_i
+    end_hour = params[:end_time].to_i
+    time_range = (start_hour..end_hour)
+    @schedule = Booking.build_schedule(dates)
+    @schedule.each_key do |date_key|
+      @schedule[date_key].each_key do |field_key|
+        @schedule[date_key][field_key].keep_if { |k,v| v == 0 && time_range.include?(k.to_i) }
+      end
+    end
+    render json: @schedule
+  end
+
+  def build_schedule_filtered
+    dates = params[:dates].split("to").map { |date| Date.parse(date) }
+    start_hour = params[:start_time].to_i
+    end_hour = params[:end_time].to_i
+    @schedule = Booking.build_schedule(dates)
   end
 end
