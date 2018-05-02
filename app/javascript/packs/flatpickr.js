5ceaf9985548958332123b561a5ba5e46c405bdf
Miguel Ferrer <miguef7@gmail.com>
2018-05-02 17:45:01 -0500

    modify time picker on search form

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -16,33 +16,3 @@ flatpickr(".datepicker", {
     theme: "light"
   })]
 });
-
-flatpickr("#starttimepicker", {
-  altInput: true,
-  enableTime: true,
-  noCalendar: true,
-  defaultHour: 17,
-  hourIncrement: 1,
-  minuteIncrement: 60,
-  plugins: [new confirmDatePlugin({
-    confirmIcon: "<i class='fa fa-check'></i>",
-    confirmText: "OK ",
-    showAlways: true,
-    theme: "light"
-  })]
-});
-
-flatpickr("#endtimepicker", {
-  altInput: true,
-  enableTime: true,
-  noCalendar: true,
-  defaultHour: 20,
-  hourIncrement: 1,
-  minuteIncrement: 60,
-  plugins: [new confirmDatePlugin({
-    confirmIcon: "<i class='fa fa-check'></i>",
-    confirmText: "OK ",
-    showAlways: true,
-    theme: "light"
-  })]
-});
