cf232dd6cc2656d72bbff107ba5528e142dbaa00
Miguel Ferrer <miguef7@gmail.com>
2018-05-01 19:54:33 -0500

    add routesfor show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :businesses, only: [:new, :create, :index, :show]
-  resources :fields, only: [:new, :create, :index]
+  resources :fields, only: [:new, :show, :create, :index]
   get "/schedule", to: "fields#schedule", as: "schedule"
 
 end
