ef081fdbc5fc4df3a8e8481b917c6bb5ac7c514f
Daniel Lopez <dlopez.10g@gmail.com>
2018-05-03 09:13:24 -0500

    fetch accessed

--- app/javascript/components/get_modal_info.js
+++ app/javascript/components/get_modal_info.js
@@ -1,17 +1,32 @@
-const modalHeader = document.querySelector(".modal-title").innerHTML
-const modalBusinessName = document.querySelector(".modal-business-name").innerHTML
-const modalBusinessName = document.querySelector(".modal-business-name").innerHTML
+const modalHeader = document.querySelector(".modal-title")
+const modalBusinessName = document.querySelector(".modal-business-name")
+const modalFieldName = document.querySelector(".modal-field-name")
 
 const ctaButtons = document.querySelectorAll(".card-cta")
 
+const sendInfoToTheModal = (cardId) => {
+      url = ('http://localhost:3000/fields/' + cardId)
+      fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+          const fieldName = `<h3> ${data.name} </h3>`;
+          modalFieldName.insertAdjacentHTML("beforeend", fieldName);
+        })
+}
 const getCardId = (event) => {
       const cardId = event.currentTarget.dataset.id;
+      sendInfoToTheModal(cardId);
     };
 
 const callGetIdOnEvent = (btn) => {
   btn.addEventListener("click", getCardId);
 };
 
-ctaButtons.forEach(callGetIdOnEvent);
+const selectCtaButtons = () => {
+  ctaButtons.forEach(callGetIdOnEvent);
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  selectCtaButtons();
+})
 
