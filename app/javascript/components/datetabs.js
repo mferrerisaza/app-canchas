e044e87dc63718a38ebfa3e43f08b550d0097902
Miguel Ferrer <miguef7@gmail.com>
2018-05-02 19:14:18 -0500

    add methods for creating cards with js and fetching info from /schedules

--- app/javascript/components/datetabs.js
+++ app/javascript/components/datetabs.js
@@ -1,3 +1,23 @@
+const fetchSchedule = (event) => {
+  const query = document.getElementById("query").value;
+  const capacityLimit = parseInt(document.getElementById("capacity_limit").value, 10);
+  const dates = event.currentTarget.dataset.date;
+  const startTime = document.getElementById("starttime").value;
+  const endTime = document.getElementById("endtime").value;
+
+  fetch(`/schedule?utf8=✓&query=${query}&capacity_limit=${capacityLimit}&dates=${dates}&start_time=${startTime}&end_time=${endTime}`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+}
+
+const generateFieldCards = (event) => {
+  document.querySelector(".cards-container").innerHTML = "";
+  const test = fetchSchedule(event);
+  console.log(test);
+}
+
 const removeTabUnderline = (element) => {
   element.classList.remove("active-date");
 }
@@ -7,6 +27,7 @@ const addTabUnderline = (event) => {
   tabs.forEach(removeTabUnderline);
   event.currentTarget.classList.add("active-date");
   document.getElementById("dropdownMenu1").innerHTML = `${event.currentTarget.innerText} <span class="caret"></span>`
+  generateFieldCards(event);
 };
 
 const addTabListeners = (element) => {
