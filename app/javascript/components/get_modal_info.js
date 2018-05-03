93de88584997801ed48afe90efa586d809dff6e9
Daniel Lopez <dlopez.10g@gmail.com>
2018-05-03 12:13:28 -0500

    Passing toggle value to the modal Ajustado

--- app/javascript/components/get_modal_info.js
+++ app/javascript/components/get_modal_info.js
@@ -8,25 +8,24 @@ const modalPrice = document.querySelector(".modal-price")
 const modalSplitableDiv = document.querySelector(".modal-splitable");
 
 const ctaButtons = document.querySelectorAll(".card-cta")
+
 const fieldsArray = [modalBusinessName, modalFieldName, modalselectedHour, modalBusinessAddress, modalSplitableDiv, modalCapacity, modalPrice ]
 
 
 const renderToggle = (status) => {
   const value = status ? "checked" : "";
-  modalSplitableDiv.insertAdjacentHTML("beforeend", `<div class="card-toggle">
-                                                    <div>
-                                                      <label class="switch">
-                                                        <input class="splitable" type="checkbox" ${value}  >
-                                                        <span class="slider round"></span>
-                                                      </label>
-                                                    </div>
-                                                  </div>`)
-  console.log(status)
+  return `<div class="card-toggle">
+            <div>
+              <label class="switch">
+                <input class="splitable" type="checkbox" ${value}>
+                <span class="slider round"></span>
+              </label>
+            </div>
+          </div>`;
 }
 
 const getToggle = (cardId) => {
-  const splitableToggleValue = document.getElementById(`toggle${cardId}`).querySelector(".splitable").checked;
-  return splitableToggleValue;
+  return document.getElementById(`toggle${cardId}`).querySelector(".splitable").checked;
 }
 
 const cleanInnerTexts = () => {
@@ -41,12 +40,11 @@ const addInnerTexts = (data, cardId) => {
   modalFieldName.insertAdjacentHTML("beforeend", `<h5> ${data.name} </h5>`);
   modalselectedHour.insertAdjacentHTML("beforeend", `27/07/2018`);
   modalBusinessAddress.insertAdjacentHTML("beforeend", `<p> ${data.business.address} </p>`);
-  modalSplitableDiv.insertAdjacentHTML("beforeend", `<p> ${renderToggle(getToggle(cardId))} </p>`);
+  modalSplitableDiv.insertAdjacentHTML("beforeend", renderToggle(getToggle(cardId)));
   modalCapacity.insertAdjacentHTML("beforeend", `<p> Número de jugadores: ${data.capacity} </p>`);
   modalPrice.insertAdjacentHTML("beforeend", `<p> $ ${data.price_cents.toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1.')} </p>`);
 
 }
-
 
 const sendInfoToTheModal = (cardId) => {
       url = ('http://localhost:3000/fields/' + cardId)
@@ -55,7 +53,7 @@ const sendInfoToTheModal = (cardId) => {
         .then((data) => {
           cleanInnerTexts();
           addInnerTexts(data, cardId);
-        })
+        });
 }
 const getCardId = (event) => {
       const cardId = event.currentTarget.dataset.id;
