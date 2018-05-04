2a4c45c1ca47364d74d80ea843faa440e32995a8
Daniel Lopez <dlopez.10g@gmail.com>
2018-05-03 20:09:14 -0500

    una eternidad despues... la tarjeta recibe el valor del splitable del modal (sorry for the long post) y la reconcha de tu madre

--- app/javascript/components/getmodalinfo.js
+++ app/javascript/components/getmodalinfo.js
@@ -1,7 +1,7 @@
 const modalBusinessName = document.querySelector(".modal-business-name")
 const modalFieldName = document.querySelector(".modal-field-name")
 const modalselectedHour = document.querySelector(".modal-selected-hour")
-const modalBusinessAddress = document.querySelector(".modal-business-addres")
+const modalBusinessAddress = document.querySelector(".modal-business-address")
 const modalSplitable = document.querySelector(".modal-sliptable")
 const modalCapacity = document.querySelector(".modal-capacity")
 const modalPrice = document.querySelector(".modal-price")
@@ -11,8 +11,15 @@ const ctaButtons = document.querySelectorAll(".card-cta")
 
 const fieldsArray = [modalBusinessName, modalFieldName, modalselectedHour, modalBusinessAddress, modalSplitableDiv, modalCapacity, modalPrice ]
 
+const addEventToModal = (cardId) => {
+    const modalToggleStatus = document.querySelector(".modal-splitable").querySelector(".splitable");
+    modalToggleStatus.addEventListener("click", (event) => {
+      const modalStatus = event.target.checked;
+      const cardToggleStatus = modalStatus ? document.querySelector(`#toggle${cardId}`).querySelector(".splitable").checked = true : document.querySelector(`#toggle${cardId}`).querySelector(".splitable").checked = false;
+    })
+};
 
-const renderToggle = (status) => {
+const renderToggle = (status, cardId) => {
   const value = status ? "checked" : "";
   return `<div class="card-toggle">
             <div>
@@ -22,39 +29,40 @@ const renderToggle = (status) => {
               </label>
             </div>
           </div>`;
-}
+};
 
 const getToggle = (cardId) => {
   return document.getElementById(`toggle${cardId}`).querySelector(".splitable").checked;
-}
+};
 
 const cleanInnerTexts = () => {
   fieldsArray.forEach((field) => {
     field.innerHTML = "";
   });
-}
+};
 
 const addInnerTexts = (data, cardId) => {
 
-  modalBusinessName.insertAdjacentHTML("beforeend", `<h3> ${data.business.name} </h3>`);
+  modalBusinessName.insertAdjacentHTML("beforeend", `<h3 dataset=${cardId}> ${data.business.name} </h3>`);
   modalFieldName.insertAdjacentHTML("beforeend", `<h5> ${data.name} </h5>`);
   modalselectedHour.insertAdjacentHTML("beforeend", `27/07/2018`);
   modalBusinessAddress.insertAdjacentHTML("beforeend", `<p> ${data.business.address} </p>`);
-  modalSplitableDiv.insertAdjacentHTML("beforeend", renderToggle(getToggle(cardId)));
+  modalSplitableDiv.insertAdjacentHTML("beforeend", renderToggle(getToggle(cardId),cardId), cardId);
   modalCapacity.insertAdjacentHTML("beforeend", `<p> Número de jugadores: ${data.capacity} </p>`);
   modalPrice.insertAdjacentHTML("beforeend", `<p> $ ${(data.price_cents/100).toFixed().replace(/(\d)(?=(\d{3})+(,|$))/g, '$1.')} </p>`);
-
-}
+  // modalSplitableDiv.setAttribute("id", `modal-card-${cardId}`)
+};
 
 const sendInfoToTheModal = (cardId) => {
-      url = ('http://localhost:3000/fields/' + cardId)
+      const url = ('/fields/' + cardId)
       fetch(url)
         .then(response => response.json())
         .then((data) => {
           cleanInnerTexts();
           addInnerTexts(data, cardId);
+          addEventToModal(cardId)
         });
-}
+};
 const getCardId = (event) => {
       const cardId = event.currentTarget.dataset.id;
       sendInfoToTheModal(cardId);
@@ -66,9 +74,8 @@ const addListenerOnClick = (btn) => {
 
 const selectCtaButtons = () => {
   ctaButtons.forEach(addListenerOnClick);
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   selectCtaButtons();
-})
-
+});
