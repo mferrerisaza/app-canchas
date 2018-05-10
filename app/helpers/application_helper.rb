59e2bc5e18a57d1df959a2dede96965e6534116f
Miguel Ferrer <miguef7@gmail.com>
2018-05-09 20:19:31 -0500

    style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,40 +31,49 @@ module ApplicationHelper
       dates = args[:dates].split('to').map { |date| Date.parse(date) }
       dates.size == 2 ? dates : dates.push(dates [0])
     else
-      dates = [Date.today, Date.today + 7]
+      [Time.zone.today, Time.zone.today + 7]
     end
   end
 
-  def generate_hours_range(args = {}, field)
-    if args[:start_time].present? && args[:start_time].to_i >= field.business.opening
+  def generate_hours_range(field, args = {})
+    start_hour = field.business.opening
+    end_hour = field.business.closing
+
+    if args[:start_time].present? && args[:start_time].to_i >= start_hour
       start_hour = args[:start_time].to_i
-    else
-      start_hour = field.business.opening
     end
 
-    if args[:end_time].present? && args[:end_time].to_i <= field.business.closing
+    if args[:end_time].present? && args[:end_time].to_i <= end_hour
       end_hour = args[:end_time].to_i
-    else
-      end_hour = field.business.closing
     end
 
-    time_range = (start_hour..end_hour)
+    (start_hour..end_hour)
   end
 
   def field_picture_or_generic(field)
+    options =
+      {
+        height: 800, width: 800,
+        crop: 'fit'
+      }
+
     if field.photo.blank?
-      cl_image_path('fieldplaceholder.jpg', height: 800, width: 800, crop: 'fit')
+      cl_image_path('fieldplaceholder.jpg', options)
     else
-      cl_image_path(field.photo, height: 800, width: 800, crop: 'fit')
+      cl_image_path(field.photo, options)
     end
   end
 
   def business_logo_or_generic(business)
+    options =
+      {
+        height: 300, width: 300, crop: :fill, gravity: :face,
+        class: 'card-top-business-logo'
+      }
     if business.photo.blank?
-      cl_image_tag('defaul-logo.png', height: 300, width: 300, crop: :fill,gravity: :face, class: 'card-top-business-logo')
+      cl_image_tag('defaul-logo.png', options)
     else
-      cl_image_tag(business.photo, height: 300, width: 300, crop: :fill,gravity: :face, class: 'card-top-business-logo')
+      cl_image_tag(business.photo, options)
     end
   end
-
 end
