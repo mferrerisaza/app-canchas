671335e91b000ba93d368adba5976c92fd12c3e1
Miguel Ferrer <miguef7@gmail.com>
2018-05-10 10:23:11 -0500

    refactor map to avoid errors and allow exporting markers function

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,18 +1,6 @@
 import GMaps from 'gmaps/gmaps.js';
 
-const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({
-    el: '#map',
-    lat: 0,
-    lng: 0,
-    idle: function(e) {
-      console.log("boundries", e.getBounds());
-      console.log("lat", e.getCenter().lat());
-      console.log("lng", e.getCenter().lng());
-    }
-  })
-
+const addMarkersToMap =(mapElement, map) => {
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
@@ -25,6 +13,28 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   }
 }
 
-import { autocomplete } from '../components/autocomplete';
-autocomplete();
+const insertMapOnDOM = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // don't try to build a map if there's no div#map to inject in
+    const map = new GMaps({
+      el: '#map',
+      lat: 0,
+      lng: 0,
+      idle: function(e) {
+        // console.log("boundries", e.getBounds());
+        // console.log("lat", e.getCenter().lat());
+        // console.log("lng", e.getCenter().lng());
+      }
+    })
+    addMarkersToMap(mapElement, map);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  insertMapOnDOM();
+})
+
+export default addMarkersToMap;
+// import { autocomplete } from '../components/autocomplete';
+// autocomplete();
 
