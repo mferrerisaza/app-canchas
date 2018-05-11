22f61eea56574e4b51b7ed012688ac356455ea23
Daniel Lopez <dlopez.10g@gmail.com>
2018-05-11 15:26:48 -0500

    Ajustes en el checkeo de si es splitadeo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -108,19 +108,21 @@ module ApplicationHelper
     end
   end
 
-  def set_paid_players(splitable, n_players, paid_players, players)
+  def set_paid_players(splitable, n_players, paid_players)
     if splitable
-      "#{count_paid_players(players)} de #{n_players} han pagado"
+      "#{paid_players} de #{n_players} han pagado"
     else
       "Pagado a la fecha"
     end
   end
 
   def count_paid_players(players)
-    players.select {|player| player.player_pay_status == true}.count
+    a = players.select { |player| player.player_pay_status == true}.count
+    a
   end
 
   def total_paid(n_paid_players, price_per_player)
+    byebug
     n_paid_players * price_per_player
   end
 
