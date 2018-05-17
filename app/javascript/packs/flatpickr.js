6a5382f1ff35865fe424c7dde44d23c4284f4eee
Miguel Ferrer <miguef7@gmail.com>
2018-05-17 18:40:31 -0500

    simplify dates picker and fix mobile responsiveness

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -2,18 +2,11 @@ import flatpickr from "flatpickr";
 import confirmDatePlugin from 'flatpickr/dist/plugins/confirmDate/confirmDate';
 
 import "flatpickr/dist/flatpickr.min.css";
-import "flatpickr/dist/themes/airbnb.css";
-import 'flatpickr/dist/plugins/confirmDate/confirmDate.css';
+// import 'flatpickr/dist/plugins/confirmDate/confirmDate.css';
+// import "flatpickr/dist/themes/airbnb.css";
 
 flatpickr(".datepicker", {
   minDate: "today",
+  altInput: true
   // maxDate: new Date().fp_incr(15),
-  altInput: true,
-  mode: "range",
-  plugins: [new confirmDatePlugin({
-    confirmIcon: "<i class='fa fa-check'></i>",
-    confirmText: "OK ",
-    showAlways: true,
-    theme: "airbnb"
-  })]
 });
