93f9ff06aa9e32da34e86d0011945c491d552a8a
Miguel Ferrer <miguef7@gmail.com>
2018-05-17 18:25:02 -0500

    delete search form js file because its no longer needed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import "bootstrap";
 import "./flatpickr.js";
 import "../components/getmodalinfo.js";
 import "../components/datetabs.js";
-import "../components/searchform.js";
 import "../components/field-card.js";
 import "./map.js"
 import "../components/slider.js";
