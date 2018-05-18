18f9d1762bc622254c76e3a43440bcff0979a3e1
Miguel Ferrer <miguef7@gmail.com>
2018-05-18 00:54:06 -0500

    submit form on date change

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -5,8 +5,11 @@ import "flatpickr/dist/flatpickr.min.css";
 // import 'flatpickr/dist/plugins/confirmDate/confirmDate.css';
 // import "flatpickr/dist/themes/airbnb.css";
 
-flatpickr(".datepicker", {
+const dateInput = flatpickr(".datepicker", {
   minDate: "today",
-  altInput: true
+  altInput: true,
   // maxDate: new Date().fp_incr(15),
+   onChange: function(selectedDates, dateStr, instance){
+    document.getElementById("dates-form").submit()
+   }
 });
