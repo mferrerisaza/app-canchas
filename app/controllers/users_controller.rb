bb1d05d6be72c8cacc279a2ee98041dc62c71588
Daniel Lopez <dlopez.10g@gmail.com>
2018-05-23 15:59:15 -0500

    go to create booking after update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,17 @@
 class UsersController < ApplicationController
-  # skip_after_action :verify_authorized
-  before_action :set_booking, :set_user
+  after_action :verify_authorized, except: %i[edit update]
+  before_action :authenticate_user!
+  before_action :set_user
 
   def edit
-    authorize @booking
     non_blank_fields = %w[first_name last_name telefono identificacion]
     return if non_blank_fields.any? { |field| @user.send(field).blank? }
-    append_booking_player(@booking, @user)
-    redirect_to bookings_path
   end
 
   def update
     @user.update(user_params)
-    authorize @booking
     if @user.save
-      append_booking_player(@booking, @user)
+      create_booking_after_sign_in(@user)
       redirect_to bookings_path
     else
       render 'edit'
@@ -25,16 +22,6 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def set_booking
-    @booking = Booking.find(params[:booking_id])
-  end
-
-  def append_booking_player(booking, user)
-    return unless booking.users.size.zero?
-    booking.booking_players << BookingPlayer.new(user: user)
-    BookingMailer.send_request(booking, user)
   end
 
   def user_params
