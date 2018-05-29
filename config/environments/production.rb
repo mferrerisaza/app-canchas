cc304ff40df3e07b0ea0e9c1a2e442f7bfef9d9e
Miguel Ferrer <miguef7@gmail.com>
2018-05-28 20:40:29 -0500

    fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ config.webpacker.check_yarn_integrity = false
   # config.action_mailer.default_url_options = { host: "scuad.co" }
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
