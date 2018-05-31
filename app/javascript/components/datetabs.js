9a13abeafffbbb9ab9d33d8bb9621ca2803e6a4e
Miguel Ferrer <miguef7@gmail.com>
2018-05-31 09:13:30 -0500

    tu-vos-usted

--- app/javascript/components/datetabs.js
+++ app/javascript/components/datetabs.js
@@ -103,7 +103,7 @@ function buildFieldCard (field, schedule, date) {
                           data-target="#myModal"
                           disabled = "disabled"
                         >
-                          NO HA SELECCIONADO NINGÚN HORARIO
+                          NO HAZ SELECCIONADO NINGÚN HORARIO
                         </button>
                       </div>
                     </div>
